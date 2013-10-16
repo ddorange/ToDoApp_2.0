85ff101946eb90a07b962a4be7ec59df89eeec24
inoue masaru <inoue_masaru@cyberagent.co.jp>
2013-10-16 21:52:08 +0900

    AppViewを起点に構成する（コントローラーメソッドしかなくて気持ち悪いけど）

--- src/asset/js/view.js
+++ src/asset/js/view.js
@@ -15,15 +15,23 @@ app.view = (function(){
         menu:   null,
         list:   null,
         detail: null,
-        events: {
-
-        },
+        
         initialize: function(){
-            // var menuModel   = new app.model.MenuModel,
-            //     collection   = new app.model.List,
-            //     detailModel = new app.model.;
+            var menuModel;
 
-            // this.menu = new app.view.MenuView( menuModel );
+            // メニューの作成
+            menuModel = new app.model.MenuModel();
+            this.menu = new app.view.MenuView( {model: menuModel} );
+
+            // 一覧画面の作成
+            this.list = new app.view.ListView( {collection: this.collection} );
+
+            // 詳細画面の作成
+            this.detail = new app.view.DetailView();
+
+            this.listenTo(this.collection, 'add', this.addOne);
+            this.listenTo(this.collection, 'reset', this.addAll);
+            this.listenTo(this.collection, 'all', this.render);
         }
     });
     /**
@@ -47,6 +55,7 @@ app.view = (function(){
         initialize: function() {
             this.$titleEl = this.$('#menu_view__title');
             this.model.on('change:state', this.changeState, this);
+            this.listenTo(this.collection, 'add', this.addNew);
         },
         /**
          *
@@ -74,18 +83,27 @@ app.view = (function(){
 
             return this;
         },
+
+        // Active Functions
         reload: function(){
             console.log('reload');
             // window.location.reload(true);
         },
+        addNew: function(){
+            console.log('addNew');
+            this.collection.createNew();
+        },
         back: function(){
             console.log('back');
         },
-        addNew: function(){
-            console.log('addNew');
-        },
         deleteOne: function(){
             console.log('deleteOne');
+        },
+
+        // Passive Functions 
+        addNew: function(model){
+            var view = new app.view.NoteView( {model: model} );
+            this.$el.append( view.render().el );
         }
     });
     /**
@@ -96,7 +114,16 @@ app.view = (function(){
      */
     ListView = Backbone.View.extend({
         el: '#list_view',
+        menu: null,
+        events: {
+
+        },
+        
         initialize: function(){
+            var menuModel = new app.model.MenuModel();
+            
+            this.menu = new app.view.MenuView( {model: menuModel} );
+
             // イベントリスナの設定
             // this.collection.on('add', this.addNew, this);
             // this.collection.on('change', this.updateCount, this);
